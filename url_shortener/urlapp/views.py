6e0d00cd6becf199e92820aaa0ab4c06245a8a2a
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-04-26 14:19:56 +0300

    url

--- url_shortener/urlapp/views.py
+++ url_shortener/urlapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import uuid
+from .models import LinkInfo
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    
     return render(request, 'index.html', {})
+
+def add(request):
+    if request.method == "POST":
+        link = request.POST['link']
+        link_id = str(uuid.uuid4())[:5]
+        new_link = LinkInfo(link=link, link_id=link_id)
+        new_link.save()
+
+        return HttpResponse(link_id)
